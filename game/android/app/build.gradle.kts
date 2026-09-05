5f476d73740c41591f9d5cb6bd7b6703b288614c
Energotron <33173005+Energotron@users.noreply.github.com>
2026-09-05 19:35:47 +0300

    build(android): target API 36 and separate debug package

--- game/android/app/build.gradle.kts
+++ game/android/app/build.gradle.kts
@@ -4,19 +4,24 @@ plugins {
 
 android {
     namespace = "com.quantdeus.spacerangers3"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.quantdeus.spacerangers3"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
         release {
             isMinifyEnabled = false
+            isDebuggable = false
         }
     }
 
